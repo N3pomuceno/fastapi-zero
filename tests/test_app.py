eba77315328422b2912bebc652167dbda471967d
João <jnfazevedo@hotmail.com>
2025-07-29 07:54:26 -0300

    Aula3: Adicionando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,25 +2,83 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from src.app import app
 
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client: TestClient):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-# Exercise:
-# 1. Create a new endpoint that returns the current date and time.
-def test_datetime_deve_retornar_ok_e_data_hora_atual():
-    client = TestClient(app)  # Arrange
+def test_create_user(client: TestClient):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
 
-    response = client.get('/datetime')  # Act
 
-    assert response.status_code == HTTPStatus.OK  # Assert
-    assert 'message' in response.json()
-    assert 'O horário atual é:' in response.json()['message']
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_update_user_failing(client):
+    response = client.put(
+        '/users/10',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_failing(client):
+    response = client.delete('/users/10')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
